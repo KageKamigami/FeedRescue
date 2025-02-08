3234eeb3110cd006f262cf0d6b97ef45cbc856ef
Andyt212 <andyt21202@gmail.com>
2025-02-08 10:20:31 -0600

    ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom"; // Import Link
 import "./App.css";
-import HomePageBanner from "./assets/HomePageBanner.png";
+import HomePageBanner from "./assets/BeachFoodBanner.png";
 
 function App() {
   return (
     <>
       {/*NAVBAR*/}
-      <nav className="flex justify-evenly">
+      <nav className="flex justify-evenly m-5">
         <div>Food Rescue</div> <Link to="/">Home</Link>
         <Link to="/about">About</Link> Sign In
       </nav>
 
       {/*BANNER*/}
-      <div className="relative w-screen h-[300px]">
+      <div className="relative w-screen h-[400px]">
         <img
           src={HomePageBanner}
           className="w-screen h-full object-cover"
           alt="Food Rescue Banner"
         />
         <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white text-center bg-black/50 p-4">
-          <h1 className="text-2xl md:text-4xl font-bold">
+          <h1 className="text-2xl md:text-4xl font-bold w-2/5">
             SCHEDULING LEFTOVER FOODS & FOOD DONATION PICKUP IS EASIER THAN EVER
           </h1>
           <h2 className="text-lg md:text-2xl font-bold mt-2">
@@ -32,10 +32,12 @@ function App() {
       </div>
 
       {/*HOW DOES IT WORK? INFO*/}
-      <div>
-        HOW DOES THIS PROCESS WORK?
-        <section>
-          <h1>1</h1>
+      <h1 className="text-blue-400 text-center">HOW DOES THIS PROCESS WORK?</h1>
+
+      <div className="flex justify-center items-center flex-col border border-red-500">
+        {" "}
+        <section className="flex justify-center items-center flex-col">
+          <h2 className="left-0">1</h2>
           <p>
             FIRST – SEE IF DONATESTUFF PICKS UP IN YOUR AREA BY ENTERING YOUR
             ZIP CODE.
